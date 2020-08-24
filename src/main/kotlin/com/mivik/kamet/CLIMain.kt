52f14a13e3fdd7864a75aa8003a2de1d4ebc2093
mivik <mivik@qq.com>
2020-08-24 19:54:42 +0800

    Slight tweak on CLI.

--- src/main/kotlin/com/mivik/kamet/CLIMain.kt
+++ src/main/kotlin/com/mivik/kamet/CLIMain.kt
@@ -2,6 +2,7 @@ package com.mivik.kamet
 
 import com.xenomachina.argparser.ArgParser
 import com.xenomachina.argparser.default
+import com.xenomachina.argparser.mainBody
 import org.bytedeco.javacpp.BytePointer
 import org.bytedeco.llvm.LLVM.LLVMTargetRef
 import org.bytedeco.llvm.global.LLVM
@@ -19,33 +20,33 @@ enum class OutputFormat(val defaultSuffix: String) {
 class KametArguments(parser: ArgParser) {
 	val outputFormat by parser.storing(
 		"-f", "--format",
-		help = "Output format of the compiling result"
+		help = "the output format of the compiling result (executable [default], ir, bitcode, object, assembly)"
 	) { OutputFormat.valueOf(toUpperCase()) }.default(OutputFormat.EXECUTABLE)
 
 	val targetTriple by parser.storing(
 		"-t", "--target",
-		help = "Specify the target triple used by LLVM"
+		help = "the target triple used by LLVM"
 	).default { LLVM.LLVMGetDefaultTargetTriple().toJava() }
-
-	val sourceFile by parser.positional(
-		"SOURCE",
-		help = "Kamet source file (.km) to compile"
-	) { File(this) }.addValidator {
-		if (!value.exists()) error("Source file \"$value\" does not exist")
-	}
 
 	val optimizationLevel by parser.storing(
 		"-O", "--opt-level",
-		help = "Optimization level"
+		help = "code optimization level (0~3)"
 	) { toInt() }.default(0).addValidator {
 		if (value < 0 || value > 3) error("Illegal optimization level: $value")
 	}
 
 	val outputFile by parser.storing(
 		"-o", "--output",
-		help = "Compiling output path"
+		help = "the path to place compiling output"
 	) { File(this) }.default { File("a" + outputFormat.defaultSuffix) }.addValidator {
 		if (value.exists() && !value.isFile) error("Compiling output path \"$value\" already exist and is not a file")
+	}
+
+	val sourceFile by parser.positional(
+		"SOURCE",
+		help = "Kamet source file (.km) to compile"
+	) { File(this) }.addValidator {
+		if (!value.exists()) error("Source file \"$value\" does not exist")
 	}
 }
 
@@ -57,62 +58,64 @@ fun initializeLLVM() {
 	LLVM.LLVMInitializeAllAsmPrinters()
 }
 
-fun main(args: Array<String>): Unit = ArgParser(args).parseInto(::KametArguments).run {
-	initializeLLVM()
-	val target = LLVMTargetRef()
-	captureError {
-		LLVM.LLVMGetTargetFromTriple(BytePointer(targetTriple), target, it)
-	}?.let { error("Failed to get target: $it") }
-	val machine = LLVM.LLVMCreateTargetMachine(
-		target,
-		BytePointer(targetTriple),
-		LLVM.LLVMGetHostCPUName(),
-		LLVM.LLVMGetHostCPUFeatures(),
-		optimizationLevel,
-		LLVM.LLVMRelocDefault,
-		LLVM.LLVMCodeModelDefault
-	)
-	val context = Context.topLevel(sourceFile.name)
-	val module = context.module
-	LLVM.LLVMSetModuleDataLayout(module, LLVM.LLVMCreateTargetDataLayout(machine))
-	LLVM.LLVMSetTarget(module, targetTriple)
-	with(context) {
-		Parser(sourceFile.readText()).parse().codegen()
-	}
-	context.runDefaultPass()
-	when (outputFormat) {
-		OutputFormat.OBJECT, OutputFormat.ASSEMBLY -> {
-			captureError {
-				LLVM.LLVMTargetMachineEmitToFile(
-					machine,
-					module,
-					BytePointer(outputFile.path),
-					if (outputFormat == OutputFormat.ASSEMBLY) LLVM.LLVMAssemblyFile
-					else LLVM.LLVMObjectFile,
-					it
-				)
-			}?.let { error("Failed to compile: $it") }
+fun main(args: Array<String>): Unit = mainBody("kamet") {
+	ArgParser(args).parseInto(::KametArguments).run {
+		initializeLLVM()
+		val target = LLVMTargetRef()
+		captureError {
+			LLVM.LLVMGetTargetFromTriple(BytePointer(targetTriple), target, it)
+		}?.let { error("Failed to get target: $it") }
+		val machine = LLVM.LLVMCreateTargetMachine(
+			target,
+			BytePointer(targetTriple),
+			LLVM.LLVMGetHostCPUName(),
+			LLVM.LLVMGetHostCPUFeatures(),
+			optimizationLevel,
+			LLVM.LLVMRelocDefault,
+			LLVM.LLVMCodeModelDefault
+		)
+		val context = Context.topLevel(sourceFile.name)
+		val module = context.module
+		LLVM.LLVMSetModuleDataLayout(module, LLVM.LLVMCreateTargetDataLayout(machine))
+		LLVM.LLVMSetTarget(module, targetTriple)
+		with(context) {
+			Parser(sourceFile.readText()).parse().codegen()
 		}
-		OutputFormat.IR ->
-			captureError {
-				LLVM.LLVMPrintModuleToFile(module, outputFile.path, it)
-			}?.let { error("Failed to compile: $it") }
-		OutputFormat.BITCODE ->
-			LLVM.LLVMWriteBitcodeToFile(module, outputFile.path)
-		OutputFormat.EXECUTABLE -> {
-			val objectFile = File.createTempFile("kamet", ".o")
-			captureError {
-				LLVM.LLVMTargetMachineEmitToFile(
-					machine,
-					module,
-					BytePointer(objectFile.path),
-					LLVM.LLVMObjectFile,
-					it
-				)
-			}?.let { error("Failed to compile: $it") }
-			val process = Runtime.getRuntime().exec(arrayOf("gcc", objectFile.path, "-o", outputFile.path))
-			process.errorStream.copyTo(System.err)
-			exitProcess(process.waitFor())
+		context.runDefaultPass()
+		when (outputFormat) {
+			OutputFormat.OBJECT, OutputFormat.ASSEMBLY -> {
+				captureError {
+					LLVM.LLVMTargetMachineEmitToFile(
+						machine,
+						module,
+						BytePointer(outputFile.path),
+						if (outputFormat == OutputFormat.ASSEMBLY) LLVM.LLVMAssemblyFile
+						else LLVM.LLVMObjectFile,
+						it
+					)
+				}?.let { error("Failed to compile: $it") }
+			}
+			OutputFormat.IR ->
+				captureError {
+					LLVM.LLVMPrintModuleToFile(module, outputFile.path, it)
+				}?.let { error("Failed to compile: $it") }
+			OutputFormat.BITCODE ->
+				LLVM.LLVMWriteBitcodeToFile(module, outputFile.path)
+			OutputFormat.EXECUTABLE -> {
+				val objectFile = File.createTempFile("kamet", ".o")
+				captureError {
+					LLVM.LLVMTargetMachineEmitToFile(
+						machine,
+						module,
+						BytePointer(objectFile.path),
+						LLVM.LLVMObjectFile,
+						it
+					)
+				}?.let { error("Failed to compile: $it") }
+				val process = Runtime.getRuntime().exec(arrayOf("gcc", objectFile.path, "-o", outputFile.path))
+				process.errorStream.copyTo(System.err)
+				exitProcess(process.waitFor())
+			}
 		}
 	}
 }
