ca2346811bd3ad8aee1802c04672a419fa64b11f
mivik <mivik@qq.com>
2020-08-26 23:27:38 +0800

    Remove index in ASTNode.

--- src/main/kotlin/com/mivik/kamet/ast/ASTNode.kt
+++ src/main/kotlin/com/mivik/kamet/ast/ASTNode.kt
@@ -5,17 +5,6 @@ import com.mivik.kamet.Value
 
 @Suppress("NOTHING_TO_INLINE")
 internal abstract class ASTNode {
-	var beginIndex: Int = -1
-	var endIndex: Int = -1
-
-	inline fun withStartIndex(location: Int) = apply {
-		this.beginIndex = location
-	}
-
-	inline fun withEndIndex(location: Int) = apply {
-		this.beginIndex = location
-	}
-
 	abstract fun Context.codegenForThis(): Value
 
 	open val returned: Boolean get() = false
